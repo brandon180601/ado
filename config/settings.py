99ef2243e6f47b07731ea9dd55b4638f30d4cc66
Brandon <brandon.silva.180@gmail.com>
2026-01-19 20:52:10 -0600

    CONFIG_CREDENTIALS

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,8 +141,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-GOOGLE_DRIVE_CREDENTIALS = os.path.join(
-    BASE_DIR, "credentials", "drive_client_secret.json"
-)
+# GOOGLE_DRIVE_CREDENTIALS = os.path.join(
+#     BASE_DIR, "credentials", "drive_client_secret.json"
+# )
+
+GOOGLE_DRIVE_CREDENTIALS = json.loads(os.getenv("GOOGLE_DRIVE_CREDENTIALS"))
+
 
 GOOGLE_DRIVE_ROOT_FOLDER = "ACCIDENTES_ADO"
